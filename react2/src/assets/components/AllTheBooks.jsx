e285f89f36739666a95eedd787cf1c9feda0ffa7
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-04 17:42:46 +0100

    button

--- react2/src/assets/components/AllTheBooks.jsx
+++ react2/src/assets/components/AllTheBooks.jsx
@@ -1,28 +1,50 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container } from "react-bootstrap";
 import fantasy from "../books/fantasy.json";
+import history from "../books/history.json";
+import horror from "../books/horror.json";
+import romance from "../books/romance.json";
+import scifi from "../books/Scifi.json";
 import { Component } from "react";
 
 class Libri extends Component {
-  state = {
-    cover: null,
-  };
+  state = { books: fantasy };
+
   render() {
     return (
-      <div className="d-flex flex-wrap justify-content-between">
-        {fantasy.map((libro) => (
-          <Card style={{ width: "18rem", margin: "10px" }} key={libro.asin}>
-            <Card.Img variant="top" src={libro.img} alt={libro.title} />
-            <Card.Body>
-              <Card.Title>{libro.title}</Card.Title>
-              <Card.Text>Categoria : {libro.category}€</Card.Text>
-              <Card.Text>{libro.price}€</Card.Text>
-              <Button variant="primary" className="btn-success">
-                Acquista
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
+      <>
+        <Container fluid>
+          <Button variant="btn-success" onClick={() => this.setState({ books: fantasy })}>
+            Fantasy
+          </Button>
+          <Button variant="btn-danger" onClick={() => this.setState({ books: horror })}>
+            Horror
+          </Button>
+          <Button variant="btn-info" onClick={() => this.setState({ books: romance })}>
+            Romance
+          </Button>
+          <Button variant="btn-dark" onClick={() => this.setState({ books: history })}>
+            History
+          </Button>
+          <Button variant="btn-warning" onClick={() => this.setState({ books: scifi })}>
+            Scifi
+          </Button>
+        </Container>
+        <div className="d-flex flex-wrap justify-content-between">
+          {this.state.books.map((libro) => (
+            <Card style={{ width: "18rem", margin: "10px" }} key={libro.asin}>
+              <Card.Img variant="top" src={libro.img} alt={libro.title} />
+              <Card.Body>
+                <Card.Title>{libro.title}</Card.Title>
+                <Card.Text>Categoria : {libro.category}€</Card.Text>
+                <Card.Text>{libro.price}€</Card.Text>
+                <Button variant="primary" className="btn-success">
+                  Acquista
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </>
     );
   }
 }
