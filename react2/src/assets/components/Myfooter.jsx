424ca70ddef013b3df7a8cbc74481b55fb94a398
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-04 17:15:39 +0100

    footer ok

--- react2/src/assets/components/Myfooter.jsx
+++ react2/src/assets/components/Myfooter.jsx
@@ -1,8 +1,17 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 function MyFooter() {
   return (
-    <>
-      <p>Questo è il footer!</p>
-    </>
+    <div className="d-flex justify-content-center bg-dark">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav className="me-auto ">
+            <Nav.Link href="#">Contattaci</Nav.Link>
+            <Nav.Link href="#">Parla con noi</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 export default MyFooter;
